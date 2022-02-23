ef2870605bec10a4b4d7370d78b602cfd47bc0fe
veryheavypickle <xavier.carroll01@gmail.com>
2022-02-24 00:21:07 +0100

    hmmmm

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -1,39 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/21 18:12:01 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/02/24 00:15:52 by xcarroll         ###   ########.fr       */
+/*   Created: 2022/02/21 19:37:15 by xcarroll          #+#    #+#             */
+/*   Updated: 2022/02/24 00:15:19 by xcarroll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	get_string_size(char *str)
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_hex(int c)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	if (c < 16)
+	{
+		ft_putchar('0');
+		ft_putchar(hex[c]);
+	}
+	else
+	{
+		print_hex(c / 16);
+		print_hex(c % 16);
+	}
+}
+
+void	ft_putstr_non_printable(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
+		if (str[i] >= 32 && str[i] <= '~')
+			ft_putchar(str[i]);
+		else
+		{
+			ft_putchar(92);
+			print_hex(str[i]);
+		}
 		i++;
 	}
-	return (i);
-}
-
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
-{
-	unsigned int	i;
-
-	i = 0;
-	if (get_string_size(dest) < size || size == 0)
-		return (0);
-	while (src[i] != '\0' && i < size - 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (get_string_size(src));
 }
